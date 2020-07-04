30c3480c40e79fb8a0605d9e4c8629835455274a
gahgsp <kelvinbianchi@gmail.com>
2020-07-03 21:18:33 -0300

    Added crouch action to the player

--- Assets/Scripts/First Person Character/CharacterMovement.cs
+++ Assets/Scripts/First Person Character/CharacterMovement.cs
@@ -7,9 +7,25 @@ public class CharacterMovement : MonoBehaviour
 
     private CharacterController _characterController;
 
-    private void Awake() => _characterController = GetComponent<CharacterController>();
+    private float _ccOriginalHeight;
+    private float _originalMoveSpeed;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        _characterController = GetComponent<CharacterController>();
+        _ccOriginalHeight = _characterController.height;
+        _originalMoveSpeed = _moveSpeed;
+    }
 
     private void Update()
+    {
+        Move();
+        Crouch();
+    }
+
+    private void Move()
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
@@ -17,5 +33,20 @@ public class CharacterMovement : MonoBehaviour
         var direction = new Vector3(horizontal, 0, vertical);
         var movement = transform.TransformDirection(direction) * _moveSpeed;
         _characterController.SimpleMove(movement);
+    }
+
+    private void Crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            _characterController.height = _ccOriginalHeight / 2;
+            _moveSpeed /= 3;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            _characterController.height = _ccOriginalHeight;
+            _moveSpeed = _originalMoveSpeed;
+        }
     }
 }
